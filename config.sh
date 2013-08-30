57c0bb6c9c5ed1b8f10116fb5677bb1ff79a1bab
Arron King <arron.king@arrowdesign.co.uk>
2013-08-30 11:45:39 +0100

    Added the config file, this sets up the config for our functions. It allows settings to be overwritten by a config.local.sh file.

---
+++ config.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# A config file, it first loads the config.local.sh file to set the variables. It then sets defaults on the variables not set.
+
+# Read the local config.local.sh
+if [ -e "config.local.sh" ]; then
+	source config.local.sh
+fi
+
+#Ok, lets set the defaults, check to see if they are set, if not then set them.
+# DATA_DIR - where we store our data
+# MYSQL_CMD - our mysql command
+# MYSQL_USER - our mysql user to login
+# MYSQL_HOST - our mysql host
+# MYSQL_PASS - our password to login with
+
+if [ -z "$DATA_DIR" ]; then
+	DATA_DIR="$HOME/.mysql-clh-data"
+fi
+
+if [ -z "$MYSQL_CMD" ]; then
+	MYSQL_CMD="mysql"
+fi
+
+if [ -z "$MYSQL_USER" ]; then
+	MYSQL_USER="root"
+fi
+
+if [ -z "$MYSQL_HOST" ]; then
+	MYSQL_HOST="localhost"
+fi
+
+if [-z "$MYSQL_PASS" ]; then
+	MYSQL_PASS=""
+fi
