2f90af085c1f56bbe7b895833596a5648373f9ea
Arron King <arron.king@arrowdesign.co.uk>
2013-08-30 12:36:06 +0100

    Added the mysqldump command to the config

--- .config.sh
+++ .config.sh
@@ -9,6 +9,7 @@ fi
 #Ok, lets set the defaults, check to see if they are set, if not then set them.
 # DATA_DIR - where we store our data
 # MYSQL_CMD - our mysql command
+# MYSQL_DUMP_CMD - our mysqldump command
 # MYSQL_USER - our mysql user to login
 # MYSQL_HOST - our mysql host
 # MYSQL_PASS - our password to login with
@@ -19,6 +20,10 @@ fi
 
 if [ -z "$MYSQL_CMD" ]; then
 	MYSQL_CMD="mysql"
+fi
+
+if [ -z "$MYSQL_DUMP_CMD" ]; then
+	MYSQL_DUMP_CMD="mysqldump"
 fi
 
 if [ -z "$MYSQL_USER" ]; then
